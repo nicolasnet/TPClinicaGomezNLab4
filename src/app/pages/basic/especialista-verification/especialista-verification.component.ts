159785cfb0350b7b4c360094426264136197cd0f
Nicolas <you@example.com>
2021-06-03 22:02:31 -0300

    arreglos varios

--- src/app/pages/basic/especialista-verification/especialista-verification.component.ts
+++ src/app/pages/basic/especialista-verification/especialista-verification.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { User } from 'src/app/clases/user';
 import { AuthFirebaseService } from 'src/app/services/auth-firebase.service';
 
 @Component({
@@ -9,11 +11,24 @@ import { AuthFirebaseService } from 'src/app/services/auth-firebase.service';
 })
 export class EspecialistaVerificationComponent implements OnInit {
 
-  public user$: Observable<any> = this.authSvc.firebaseAuth.user;
+  
+  public user$: Observable<User> = this.authSvc.firebaseAuth.user;
+  public role: string = localStorage.getItem('role');
+  usuario: User;
 
-  constructor(private authSvc: AuthFirebaseService) {}
+  constructor(private navegador: Router, private authSvc: AuthFirebaseService) {}
 
   ngOnInit(): void {
+  }
+
+  Navegar(ruta: string){
+    this.navegador.navigate([ruta]);
+  }
+
+  Desconectarse(){
+    this.authSvc.LogOut();
+    this.role = "";
+    this.Navegar("");
   }
 
 }
